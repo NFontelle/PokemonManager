01bea2b2ce41d2e8756c05285a1c252a2e50a492
Nathalie F <gogs@fake.local>
2020-01-23 11:17:50 +0100

    commit TP4 bis

--- src/app/composants/pokemons/pokemons.component.ts
+++ src/app/composants/pokemons/pokemons.component.ts
@@ -9,12 +9,12 @@ export class PokemonsComponent implements OnInit {
 
 
   private pokemons = [
-    { id : 1, nom : 'Bulbizarre', cathegorie : 'graine', type : ['plante, poison'], taille : 0.70, poid : 6.9},
-    { id : 2, nom : 'Herbizarre', cathegorie : 'graine', type : ['plante, poison'], taille : 1.00, poid : 13.0},
-    { id : 3, nom : 'Florizarre', cathegorie : 'graine', type : ['plante, poison'], taille : 0.70, poid : 6.9},
-    { id : 4, nom : 'Salamèche', cathegorie : 'lézard', type : ['feu'], taille : 0.6, poid : 8.5},
-    { id : 5, nom : 'Reptincel', cathegorie : 'lézard', type : ['feu'], taille : 1.1, poid : 19.0},
-    { id : 6, nom : 'Dracaufeu', cathegorie : 'flamme', type : ['feu, vol'], taille : 1.70, poid : 90.5}
+    { id : 1, nom : 'Bulbizarre', categorie : 'graine', type : ['plante, poison'], taille : 0.70, poids : 6.9},
+    { id : 2, nom : 'Herbizarre', categorie : 'graine', type : ['plante, poison'], taille : 1.00, poids : 13.0},
+    { id : 3, nom : 'Florizarre', categorie : 'graine', type : ['plante, poison'], taille : 0.70, poids : 6.9},
+    { id : 4, nom : 'Salamèche', categorie : 'lézard', type : ['feu'], taille : 0.6, poids : 8.5},
+    { id : 5, nom : 'Reptincel', categorie : 'lézard', type : ['feu'], taille : 1.1, poids : 19.0},
+    { id : 6, nom : 'Dracaufeu', categorie : 'flamme', type : ['feu, vol'], taille : 1.70, poids : 90.5}
   ];
 
 
